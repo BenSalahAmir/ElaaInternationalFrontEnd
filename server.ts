25918d2abec876c4d4ec7c7696a67ac1d6ba3cd5
amir ben salah <amir.bensalah@esprit.tn>
2024-05-16 04:40:27 +0200

    update port num 10000

--- server.ts
+++ server.ts
@@ -44,7 +44,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 3000; // Use PORT environment variable or default to 4000
+  const port = process.env['PORT'] || 10000; // Use PORT environment variable or default to 4000
 
   // Start up the Node server
   const server = app();
